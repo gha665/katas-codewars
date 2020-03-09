e3f78e15a9db0e96d718ba25eea23f70598a22f7
Evgeny Galitskiy <gha665@gmail.com>
2020-03-08 21:22:23 -0400

    added is-europeans kata

---
+++ is-europeans.js
@@ -0,0 +1,19 @@
+const list1 = [
+	{ firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+	{ firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+	{ firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+	{ firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+];
+
+const list2 = [
+	{ firstName: 'Oliver', lastName: 'Q.', country: 'Australia', continent: 'Oceania', age: 19, language: 'HTML' },
+	{ firstName: 'Lukas', lastName: 'R.', country: 'Austria', continent: 'Europe', age: 89, language: 'HTML' }
+];
+
+function countDevelopers(list) {
+	return list.filter((isFromEurope) => {
+		return isFromEurope.continent === 'Europe' && isFromEurope.language === 'JavaScript';
+	}).length;
+}
+
+console.log(countDevelopers(list1, list2));
