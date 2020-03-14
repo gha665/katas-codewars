1491882213a19d1a5c32b0e797aea74b4b56dc90
Evgeny Galitskiy <gha665@gmail.com>
2020-03-14 11:06:39 -0400

    added is-ruby-coming kata

--- greet-developers.js
+++ greet-developers.js
@@ -15,7 +15,7 @@ var list1 = [
 ];
 
 function greetDevelopers(list) {
-	return list.map((dev) => {
+	return list.map(dev => {
 		dev.greeting = `Hi ${dev.firstName}, what do you like the most about ${dev.language}?`;
 	});
 	return dev;
---
+++ is-ruby-coming.js
@@ -0,0 +1,11 @@
+var list1 = [
+    { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+    { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+    { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+  ];
+
+  
+  const isRubyComing list => {
+    return list.some(dev => dev.language === "Ruby")
+  }
+  isRubyComing(list1); 
