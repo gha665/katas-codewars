34b5508b45aecafa8eb3d46a87e8e9c1922ffcb2
Evgeny Galitskiy <gha665@gmail.com>
2020-06-11 23:46:34 -0400

    added disemvowel kata

---
+++ disemvowel-trolls.js
@@ -0,0 +1,26 @@
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+// Note: for this kata y isn't considered a vowel.
+
+let phrase = "This website is for losers LOL!";
+
+let phrase = "This website is for losers LOL!";
+
+function disemvowel(str) {
+  const vowels = ["a", "e", "i", "o", "u"];
+
+  return str
+    .split("")
+    .filter(function (el) {
+      return vowels.indexOf(el.toLowerCase()) == -1;
+    })
+    .join("");
+}
+
+disemvowel(phrase);
+
+// function disemvowel(str) {
+//   string = str.map((x) => x.replace(/[aeiou]/g, ""));
+// }
+
+// disemvowel(phrase);
